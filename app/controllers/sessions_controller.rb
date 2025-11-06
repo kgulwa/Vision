646cadcde9197a88a0702a3a090b694772180e3d
kgulwa <konkecodes@LAPTOP-JNVMN2GL.localdomain>
2025-11-06 09:04:27 +0200

    Set up homepage for Vision with a working root route, fixed SessionsController syntax

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+  def home
+    # homepage
+  end
+
+  def new
+    # login form
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Welcome back, #{@user.username}!"
+    else
+      flash.now[:alert] = "Something went wrong! Make sure your username and password are correct"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Logged out successfully"
+  end
+
+end
