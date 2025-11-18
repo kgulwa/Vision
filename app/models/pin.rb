37d16434af3f2f04f74bc4ef1984ac3fd8d2ce32
kgulwa <konkecodes@LAPTOP-JNVMN2GL.localdomain>
2025-11-18 16:33:38 +0200

    Reposts now show in user profile

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,14 +2,9 @@ class Pin < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  # Comments
   has_many :comments, dependent: :destroy
-
-  # Likes
   has_many :likes, dependent: :destroy
   has_many :likers, through: :likes, source: :user
-
-  # Reposts
   has_many :reposts, dependent: :destroy
   has_many :reposted_by_users, through: :reposts, source: :user
 
@@ -17,6 +12,11 @@ class Pin < ApplicationRecord
   validates :image, presence: true
 
   scope :recent, -> { order(created_at: :desc) }
+
+  # Count methods
+  def comments_count
+    comments.count
+  end
 
   def likes_count
     likes.count
