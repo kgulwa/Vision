a924d1179dd9f009e8743372ff805089bb0d7323
kgulwa <konkecodes@LAPTOP-JNVMN2GL.localdomain>
2025-12-15 15:44:03 +0200

    fixed my paths

---
+++ app/services/likes/create.rb
@@ -0,0 +1,48 @@
+module Likes
+  class Create
+    def self.call(user:, pin:)
+      new(user, pin).call
+    end
+
+    def initialize(user, pin)
+      @user = user
+      @pin = pin
+    end
+
+    def call
+      user.like(pin)
+      notify_pin_owner
+      notify_tagged_users
+    end
+
+    private
+
+    attr_reader :user, :pin
+
+    def notify_pin_owner
+      return if pin.user.uid == user.uid
+
+      Notification.create!(
+        user: pin.user,
+        actor: user,
+        action: "liked your post",
+        notifiable: pin,
+        read: false
+      )
+    end
+
+    def notify_tagged_users
+      pin.tagged_users.each do |tagged|
+        next if tagged.id == user.id
+
+        Notification.create!(
+          user: tagged,
+          actor: user,
+          action: "liked a post you're tagged in",
+          notifiable: pin,
+          read: false
+        )
+      end
+    end
+  end
+end
