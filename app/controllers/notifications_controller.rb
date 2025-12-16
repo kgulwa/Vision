9bb97177aaa1765c2c6719e890f3ef7ad5f69272
kgulwa <konkecodes@LAPTOP-JNVMN2GL.localdomain>
2025-12-16 20:08:09 +0200

    Refactored my notifications to use services

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,10 +3,9 @@ class NotificationsController < ApplicationController
 
   def index
     @notifications = current_user.notifications
-                                .includes(:actor, :notifiable)
-                                .order(created_at: :desc)
+                                 .includes(:actor, :notifiable)
+                                 .order(created_at: :desc)
 
-    # Mark all as read when viewed
-    current_user.notifications.where(read: false).update_all(read: true)
+    Notifications::MarkAllAsRead.call(user: current_user)
   end
 end
